4d0da6b862d3d88de29c2d3f2dc8d06d477352f6
Nyuk <gillbanks.nate@gmail.com>
2019-10-17 11:06:52 +1100

    TestAgain

---
+++ app/src/main/java/com/example/carparkdemo/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.carparkdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    private EditText emailt;
+    private EditText passwordt;
+    private Button logint;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//
+        emailt = (EditText)findViewById(R.id.email);
+        passwordt = (EditText)findViewById(R.id.password);
+        logint = (Button)findViewById(R.id.btnLogin);
+
+        logint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { validate(emailt.getText().toString(), passwordt.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword)
+    {
+        if((userName.equals("admin")) && (userPassword.equals("password")))
+        {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
