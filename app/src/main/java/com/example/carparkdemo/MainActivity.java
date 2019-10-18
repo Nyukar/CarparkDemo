5c312ba8c2232d2d64e8fc9c0d20b0a501253a26
Nyuk <gillbanks.nate@gmail.com>
2019-10-19 08:08:19 +1100

    Final

--- app/src/main/java/com/example/carparkdemo/MainActivity.java
+++ app/src/main/java/com/example/carparkdemo/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         resultsView = (TextView) findViewById(R.id.FoundTextTV);
         userView = (TextView) findViewById(R.id.userNameText);
         goMap = (Button) findViewById(R.id.checkMap);
-        //goMap.setEnabled(false);
+        goMap.setEnabled(false);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
